ae2825bf1aa58e4437c2db49e9f9a13c8ade3466
charlie4pugking <48207796+charlie4pugking@users.noreply.github.com>
2019-03-04 20:42:20 +0000

    Add files via upload

---
+++ mystatus.py
@@ -0,0 +1,26 @@
+from flask import Flask
+import socket
+import multiprocessing
+from psutil import virtual_memory
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/status")
+def mystatus():
+    hostname = socket.gethostname()
+    ip = socket.gethostbyname(socket.gethostname())
+    amount = multiprocessing.cpu_count()
+    mem = virtual_memory()
+    ram = round((mem.total/1000000000), 2)
+
+    mylist = {"Hostname": hostname, "IP Address": ip, "CPUS": amount, "Memory": ram}
+
+    jsonlist = json.dumps(mylist)
+
+    return jsonlist
+
+
+if __name__ == "__main__":
+    app.run()
