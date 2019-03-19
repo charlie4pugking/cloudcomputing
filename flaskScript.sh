3235561067fb53d27c1bef13c123dae2fe21f4bd
charlie4pugking <48207796+charlie4pugking@users.noreply.github.com>
2019-03-19 18:06:36 +0000

    Update flaskScript.sh

--- flaskScript.sh
+++ flaskScript.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-
 # Update repositories
 sudo apt-get update
-
 # Install pip
 sudo apt install python3-pip
 sudo apt install python-pip
-
 # Install Flask
 pip install Flask
-
 # Install psutil to get system information
 pip install psutil
-
 # Run Python file
 python mystatus.py
